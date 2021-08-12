7991e6e15f4d687500a00a1678e790e4ef3ad412
I.D <daisuke.workin@gmail.com>
2021-08-12 18:24:14 +0900

    update

--- a.py
+++ a.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 
+
 a = np.array([[0, 1, 2, 3],
        [4, 5, 6, 7],
        [8, 9, 10, 11]])
 b = np.array(a < 5)
 
+d = np.array([0, 1, 2, 3])
+
 c = np.array([l[:2] for l in a[1:3]])
 
-print(c)
-print(np.sum(c))
-print(np.sum(a < 5))
+e = np.ravel(a)
+
+print(len(e))
